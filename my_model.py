36e1cf2f711005ba3d5c792df5e2f4ec33386dd8
ErfanGhanizadeh <eghanizadeh4@gmail.com>
2025-10-04 14:48:21 -0700

    Add files via upload

---
+++ my_model.py
@@ -0,0 +1,54 @@
+def show_menu():
+    print("options:\n"
+          "1) please enter the number\n"  
+          "2) please enter the name\n"     
+          "3) show the list of numbers\n"
+          "4) show the list of names\n"
+          "5) show the result of numbers\n"
+          "6) remove an item\n"
+          "7) exit")
+    choose = int(input("please enter the number: "))
+    return choose
+
+
+list_of_the_number = []
+list_of_the_name = []
+
+
+def add_number():
+    number = int(input("please enter the number: "))
+    list_of_the_number.append(number)
+    return number
+
+
+def add_name():
+    name = input("please enter the name: ")
+    list_of_the_name.append(name)
+    return name
+
+
+def show_the_list_number():
+    print("your current list of numbers:", list_of_the_number)
+
+
+def show_the_result_number():
+    result = sum(list_of_the_number) / len(list_of_the_number)
+    print("result is:", result)
+    return result
+
+
+def show_the_list_name():
+    print("your current list of names:", list_of_the_name)
+
+
+def remove_item():
+    choose = input("please input which list do you want to remove from (numbers/names): ")
+    if choose== "numbers":
+        remove_the_item = int(input("please enter the number you want to remove: "))
+        list_of_the_number.remove(remove_the_item)
+
+    elif choose== "names":
+        remove_the_item = input("please enter the name you want to remove: ")
+        list_of_the_name.remove(remove_the_item)
+    else:
+        print("invalid choice.")
---
+++ user_info.py
@@ -0,0 +1,30 @@
+from my_model import *
+
+while True:
+    first_attempt = show_menu()
+
+    if first_attempt == 1:
+        first_performance=add_number()
+        print("Result:", first_performance)
+        print("*" * 50)
+    elif first_attempt == 2:
+        second_performance=add_name()
+        print("Result:", second_performance)
+        print("*" * 50)
+    elif first_attempt == 3:
+        show_the_list_number()
+        print("*" * 50)
+    elif first_attempt == 4:
+        show_the_list_name()
+        print("*" * 50)
+    elif first_attempt == 5:
+        show_the_result_number()
+        print("*" * 50)
+    elif first_attempt == 6:
+        remove_item()
+        print("*" * 50)
+    elif first_attempt == 7:
+        print("Goodbye!")
+        break
+    else:
+        print("invalid number!!!")
